bb7c12759c62b801f08c0855521a9c0f0a272eb9
Tulio Alves <tulioalvescor@gmail.com>
2026-07-30 15:14:47 -0300

    Task Model and Controller

---
+++ Controllers/TaksController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TaskSystem.Models;
+
+namespace TaskSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaksController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<TaskModel>> GetAll()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ Models/TaskModel.cs
@@ -0,0 +1,12 @@
+﻿namespace TaskSystem.Models
+{
+    public class TaskModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Status { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+    }
+}
