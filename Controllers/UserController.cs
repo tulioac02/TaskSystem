2491a70347ae2fe3b7389b1a29268df026ff03c4
Tulio Alves <tulioalvescor@gmail.com>
2026-07-30 15:00:51 -0300

    User Model and Controller

---
+++ Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TaskSystem.Models;
+
+namespace TaskSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<UserModel>> GetAll()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ Models/UserModel.cs
@@ -0,0 +1,11 @@
+﻿namespace TaskSystem.Models
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set;  }
+    }
+}
